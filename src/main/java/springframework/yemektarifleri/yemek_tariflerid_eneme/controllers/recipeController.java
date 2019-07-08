9f3d6ad07bf8c67b66e7ae65d946542f9ff216db
cryptolog <irfan.simsek.gop@gmail.com>
2019-07-08 12:51:28 +0200

    RecipeDetail.html and recipeCreateAndUpdate.html were added and recipe controller was rearranged for recipe update

--- src/main/java/springframework/yemektarifleri/yemek_tariflerid_eneme/controllers/recipeController.java
+++ src/main/java/springframework/yemektarifleri/yemek_tariflerid_eneme/controllers/recipeController.java
@@ -2,7 +2,12 @@ package springframework.yemektarifleri.yemek_tariflerid_eneme.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.yemektarifleri.yemek_tariflerid_eneme.models.Recipe;
 import springframework.yemektarifleri.yemek_tariflerid_eneme.services.RecipeService;
 
 @Controller
@@ -15,10 +20,27 @@ public class recipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping({"/recipies", "/recipiesHome", "/recipieshome"})
+    @RequestMapping("/recipes")
     public  String findAllRecipe(Model model){
         model.addAttribute("recipes", recipeService.findAll());
         return "recipeHome";
+    }
+
+    @GetMapping("/recipes/{recipeId}/edit")
+    public String initUpdateOwnerForm(@PathVariable("recipeId") Long recipeId, Model model) {
+        Recipe recipe = this.recipeService.findById(recipeId);
+        model.addAttribute(recipe);
+        return "recipeUpdateAndCreate";
+    }
+
+    @PostMapping("/recipes/{recipeId}/edit")
+    public String processUpdateOwnerForm(Recipe recipe, BindingResult result, @PathVariable("recipeId") Long recipeId) {
+        if (result.hasErrors()) {
+            return "recipeUpdateAndCreate";
+        } else {
+            this.recipeService.save(recipe);
+            return "/recipes/{recipeId}";
+        }
     }
 }
 
